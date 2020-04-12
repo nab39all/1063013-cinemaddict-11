b86fea9c2bb32d4b966548708325cf2810d2d62a
Dima <pikus.dima25@gmail.com>
2020-04-12 19:10:31 +0300

    module1-task3

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 'use strict';
 const CARDS_COUNT = 5; // кол-во отрисованых карточек
-// const CARDS_COUNT_EXTRA = 2; // кол-во карточек в блоках «Top rated» и «Most commented»
-// const CONTAINERS_COUNT_EXTRA = 2; // кол-во контейнеров с карточками
+const CARDS_COUNT_EXTRA = 2; // кол-во карточек в блоках «Top rated» и «Most commented»
 
 const createHeaderProfileTemplate = () => {
   return (
     `<section class="header__profile profile">
-      <p class="profile__rating">#PROFILE RAITING</p>
-      <img class="profile__avatar" src="#" alt="Avatar" width="35" height="35">
+      <p class="profile__rating">Movie Buff</p>
+      <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
     </section>`
   );
 };
@@ -29,16 +28,16 @@ const createMainMenuTemplate = () => {
 const createFilmCardTemplate = () => {
   return (
     `<article class="film-card">
-      <h3 class="film-card__title">#FILM TITLE</h3>
-      <p class="film-card__rating">#FILM RATING</p>
+      <h3 class="film-card__title">The Dance of Life</h3>
+      <p class="film-card__rating">8.3</p>
       <p class="film-card__info">
-        <span class="film-card__year">#YEAR</span>
-        <span class="film-card__duration">#DURATION</span>
-        <span class="film-card__genre">#GENRE</span>
+        <span class="film-card__year">1929</span>
+        <span class="film-card__duration">1h 55m</span>
+        <span class="film-card__genre">Musical</span>
       </p>
-      <img src="#" alt="#" class="film-card__poster">
-      <p class="film-card__description">#DESCRIPTION</p>
-      <a class="film-card__comments">#COMMENTS</a>
+      <img src="./images/posters/the-dance-of-life.jpg" alt="" class="film-card__poster">
+      <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
+      <a class="film-card__comments">5 comments</a>
       <form class="film-card__controls">
         <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
         <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
@@ -64,59 +63,59 @@ const createFilmDetailsPopUpTemplate = () => {
           </div>
           <div class="film-details__info-wrap">
             <div class="film-details__poster">
-              <img class="film-details__poster-img" src="#" alt="">
+              <img class="film-details__poster-img" src="./images/posters/the-great-flamarion.jpg" alt="">
 
-              <p class="film-details__age">#AGE</p>
+              <p class="film-details__age">18+</p>
             </div>
 
             <div class="film-details__info">
               <div class="film-details__info-head">
                 <div class="film-details__title-wrap">
-                  <h3 class="film-details__title">#TITLE</h3>
-                  <p class="film-details__title-original">#ORIGINAL TITLE</p>
+                  <h3 class="film-details__title">The Great Flamarion</h3>
+                  <p class="film-details__title-original">Original: The Great Flamarion</p>
                 </div>
 
                 <div class="film-details__rating">
-                  <p class="film-details__total-rating">#RATING</p>
+                  <p class="film-details__total-rating">8.9</p>
                 </div>
               </div>
 
               <table class="film-details__table">
                 <tr class="film-details__row">
-                  <td class="film-details__term">#AUTHORS</td>
-                  <td class="film-details__cell">#AUTHORS</td>
+                  <td class="film-details__term">Director</td>
+                  <td class="film-details__cell">Anthony Mann</td>
                 </tr>
                 <tr class="film-details__row">
-                  <td class="film-details__term">#WRITERS</td>
-                  <td class="film-details__cell">#WRITERS</td>
+                  <td class="film-details__term">Writers</td>
+                  <td class="film-details__cell">Anne Wigton, Heinz Herald, Richard Weil</td>
                 </tr>
                 <tr class="film-details__row">
-                  <td class="film-details__term">#ACTORS</td>
-                  <td class="film-details__cell">#ACTORS</td>
+                  <td class="film-details__term">Actors</td>
+                  <td class="film-details__cell">Erich von Stroheim, Mary Beth Hughes, Dan Duryea</td>
                 </tr>
                 <tr class="film-details__row">
-                  <td class="film-details__term">#RELISE</td>
-                  <td class="film-details__cell">#RELISE</td>
+                  <td class="film-details__term">Release Date</td>
+                  <td class="film-details__cell">30 March 1945</td>
                 </tr>
                 <tr class="film-details__row">
-                  <td class="film-details__term">#RUNTIME</td>
-                  <td class="film-details__cell">#RUNTIME</td>
+                  <td class="film-details__term">Runtime</td>
+                  <td class="film-details__cell">1h 18m</td>
                 </tr>
                 <tr class="film-details__row">
-                  <td class="film-details__term">#COUNTRY</td>
-                  <td class="film-details__cell">#COUNTRY</td>
+                  <td class="film-details__term">Country</td>
+                  <td class="film-details__cell">USA</td>
                 </tr>
                 <tr class="film-details__row">
-                  <td class="film-details__term">#GENRES</td>
+                  <td class="film-details__term">Genres</td>
                   <td class="film-details__cell">
-                    <span class="film-details__genre">#GENRE</span>
-                    <span class="film-details__genre">#GENRE</span>
-                    <span class="film-details__genre">#GENRE</span></td>
+                    <span class="film-details__genre">Drama</span>
+                    <span class="film-details__genre">Film-Noir</span>
+                    <span class="film-details__genre">Mystery</span></td>
                 </tr>
               </table>
 
               <p class="film-details__film-description">
-                #FILM DESCRIPTION
+                The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Great Flamarion (Erich von Stroheim) is an arrogant, friendless, and misogynous marksman who displays his trick gunshot act in the vaudeville circuit. His show features a beautiful assistant, Connie (Mary Beth Hughes) and her drunken husband Al (Dan Duryea), Flamarion's other assistant. Flamarion falls in love with Connie, the movie's femme fatale, and is soon manipulated by her into killing her no good husband during one of their acts.
               </p>
             </div>
           </div>
@@ -135,18 +134,57 @@ const createFilmDetailsPopUpTemplate = () => {
 
         <div class="form-details__bottom-container">
           <section class="film-details__comments-wrap">
-            <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">#COMMENTS COUNT</span></h3>
+            <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">4</span></h3>
 
             <ul class="film-details__comments-list">
               <li class="film-details__comment">
                 <span class="film-details__comment-emoji">
-                  <img src="#" width="55" height="55" alt="emoji-smile">
+                  <img src="./images/emoji/smile.png" width="55" height="55" alt="emoji-smile">
                 </span>
                 <div>
-                  <p class="film-details__comment-text">#COMMENT TEXT</p>
+                  <p class="film-details__comment-text">Interesting setting and a good cast</p>
                   <p class="film-details__comment-info">
-                    <span class="film-details__comment-author">#COMMENT AUTHOR</span>
-                    <span class="film-details__comment-day">#COMMENT DAY</span>
+                    <span class="film-details__comment-author">Tim Macoveev</span>
+                    <span class="film-details__comment-day">2019/12/31 23:59</span>
+                    <button class="film-details__comment-delete">Delete</button>
+                  </p>
+                </div>
+              </li>
+              <li class="film-details__comment">
+                <span class="film-details__comment-emoji">
+                  <img src="./images/emoji/sleeping.png" width="55" height="55" alt="emoji-sleeping">
+                </span>
+                <div>
+                  <p class="film-details__comment-text">Booooooooooring</p>
+                  <p class="film-details__comment-info">
+                    <span class="film-details__comment-author">John Doe</span>
+                    <span class="film-details__comment-day">2 days ago</span>
+                    <button class="film-details__comment-delete">Delete</button>
+                  </p>
+                </div>
+              </li>
+              <li class="film-details__comment">
+                <span class="film-details__comment-emoji">
+                  <img src="./images/emoji/puke.png" width="55" height="55" alt="emoji-puke">
+                </span>
+                <div>
+                  <p class="film-details__comment-text">Very very old. Meh</p>
+                  <p class="film-details__comment-info">
+                    <span class="film-details__comment-author">John Doe</span>
+                    <span class="film-details__comment-day">2 days ago</span>
+                    <button class="film-details__comment-delete">Delete</button>
+                  </p>
+                </div>
+              </li>
+              <li class="film-details__comment">
+                <span class="film-details__comment-emoji">
+                  <img src="./images/emoji/angry.png" width="55" height="55" alt="emoji-angry">
+                </span>
+                <div>
+                  <p class="film-details__comment-text">Almost two hours? Seriously?</p>
+                  <p class="film-details__comment-info">
+                    <span class="film-details__comment-author">John Doe</span>
+                    <span class="film-details__comment-day">Today</span>
                     <button class="film-details__comment-delete">Delete</button>
                   </p>
                 </div>
@@ -195,17 +233,16 @@ const createFilmListTemplate = () => {
       <section class="films-list">
         <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
       </section>
+
+      <section class="films-list--extra">
+        <h2 class="films-list__title">Top rated</h2>
+      </section>
+      <section class="films-list--extra">
+        <h2 class="films-list__title">Most commented</h2>
+      </section>
     </section>`
   );
 };
-
-// const createExtraFilmListTemplate = () => {
-//   return (
-//     `<section class="films-list--extra">
-//       <h2 class="films-list__title">#TITLE</h2>
-//     </section>`
-//   );
-// };
 
 const createFilmCardsContainerTemplate = () => {
   return (
@@ -213,17 +250,16 @@ const createFilmCardsContainerTemplate = () => {
   );
 };
 
-const render = (container, template, place) => {
+const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
 
 const siteMain = document.querySelector(`.main`);
 const siteHeader = document.querySelector(`.header`);
-
 render(siteHeader, createHeaderProfileTemplate(), `beforeend`);
 render(siteMain, createMainMenuTemplate(), `beforeend`);
-
 render(siteMain, createFilmListTemplate(), `beforeend`);
+
 const filmsList = siteMain.querySelector(`.films-list`);
 render(filmsList, createFilmCardsContainerTemplate(), `beforeend`);
 const mainFilmsListContainer = filmsList.querySelector(`.films-list__container`);
@@ -231,15 +267,16 @@ for (let i = 0; i < CARDS_COUNT; i++) {
   render(mainFilmsListContainer, createFilmCardTemplate(), `beforeend`);
 }
 render(filmsList, createShowMoreBtnTemplate(), `beforeend`);
-// const filmsContainer = siteMain.querySelector(`.films`);
-// for (let i = 0; i < CONTAINERS_COUNT_EXTRA; i++) {
-//   render(filmsContainer, createExtraFilmListTemplate(), `beforeend`);
-// }
-// const filmsListExtra = filmsContainer.querySelectorAll(`.films-list--extra`);
-// render(filmsListExtra, createFilmCardsContainerTemplate(), `beforeend`);
-// const filmsListExtraContainer = filmsListExtra.querySelector(`.films-list__container`);
-// for (let i = 0; i < CARDS_COUNT_EXTRA; i++) {
-//   render(filmsListExtraContainer, createFilmCardTemplate(), `beforeend`);
-// }
+
+const extraFilmsList = siteMain.querySelectorAll(`.films-list--extra`);
+const [topRatedFilmsList, mostCommentedFilmsList] = extraFilmsList;
+render(topRatedFilmsList, createFilmCardsContainerTemplate(), `beforeend`);
+render(mostCommentedFilmsList, createFilmCardsContainerTemplate(), `beforeend`);
+const topRatedFilmsListContainer = topRatedFilmsList.querySelector(`.films-list__container`);
+const mostCommentedFilmsListContainer = mostCommentedFilmsList.querySelector(`.films-list__container`);
+for (let i = 0; i < CARDS_COUNT_EXTRA; i++) {
+  render(topRatedFilmsListContainer, createFilmCardTemplate(), `beforeend`);
+  render(mostCommentedFilmsListContainer, createFilmCardTemplate(), `beforeend`);
+}
 
 render(siteMain, createFilmDetailsPopUpTemplate(), `beforeend`);
